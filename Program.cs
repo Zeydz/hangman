c7efba4f757f4d608525dc5595e6d66f540a4646
Jocke <joakim03moller@gmail.com>
2024-10-16 16:21:17 +1000

    Logik för Program.cs (programstart) fixat.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿class Program 
+﻿using System;
+
+class Program
 {
+    /* Huvudmetoden där programmet börjar köras */
     static void Main(string[] args)
     {
-        /* Skapar en instans av klassen word. Returnerar ett slumpmässigt ord*/
+        /* Skapar en instans av Word-klassen för att hantera ord. Hämtar ett slumpmässigt tal */
         Word word = new Word();
         string randomWord = word.GetRandomWord();
+
+        Console.Clear();
         Console.WriteLine("Slumpmässigt ord: " + randomWord);
+
+        /* Skapar en instans av Game-klassen. Skickar in det slumpmässiga ordet. */
+        Game game = new Game(randomWord);
+        /* Startar spelet */
+        game.Start();
     }
 }
